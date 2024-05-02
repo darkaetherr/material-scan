1ac531f82ae00de22357b1e8a2f03ee729813edb
The0x9 <140558680+darkaetherr@users.noreply.github.com>
2024-05-02 15:32:01 -0700

    Update dumper.cpp

--- material/mat_scan/dumper.cpp
+++ material/mat_scan/dumper.cpp
@@ -5,8 +5,8 @@
 using nlohmann::json;
 
 int main( ) {
-	p9::memory::setup( );
-	auto game_assembly = p9::memory::get_module( L"GameAssembly.dll" );
+	mem::memory::setup( );
+	auto game_assembly = mem::memory::get_module( L"GameAssembly.dll" );
 	std::ifstream input( "json.txt" );
 	json swag;
 	input >> swag;
@@ -20,11 +20,11 @@ int main( ) {
 		iss >> std::hex >> value;
 		value -= 0x88;
 		char _name[ 255 ];
-		auto _addy = p9::memory::read<uintptr_t>( value + 0x30 );
-		p9::memory::read( _addy, _name, 255 );
+		auto _addy = mem::memory::read<uintptr_t>( value + 0x30 );
+		mem::memory::read( _addy, _name, 255 );
 		_name[ 254 ] = 0;
 		std::string name( _name );
-		auto key = p9::memory::read<uint32_t>( value + 0x8 );
+		auto key = mem::memory::read<uint32_t>( value + 0x8 );
 		output << "material name" << name << "material address" << key << "\n";
 	}
 	output.close( );
